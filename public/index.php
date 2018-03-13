980c858a0c7e78c91c1d95f82914cc773c6e7fc4
Edwin Horneij <edwin@astekwallcovering.com>
2018-03-13 14:25:55 -0700

    Add layout, use Bootstrap 3

--- public/index.php
+++ public/index.php
@@ -7,7 +7,13 @@ $request = new Fram\Request();
 
 $controllerName = implode('', array_map('ucwords', explode('_', $request->getController()))).'Controller';
 $controller = new $controllerName($request);
+
+ob_start();
 $controller->{$request->getAction()}();
+$content = ob_get_contents();
+ob_end_clean();
+
+require_once APP_DIR.'/views/layouts/application.phtml';
 
 //try {
 //    $request = new Fram\Request();
